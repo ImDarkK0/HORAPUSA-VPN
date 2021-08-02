12716f5be3c7f072b12c039e8ad5bcbe2109b50a
Bhathiya Gamage <73831309+BHATHIYA-ROOT@users.noreply.github.com>
2021-08-02 09:23:54 -0400

    Add files via upload

---
+++ Files/Menu/v2rayy.sh
@@ -0,0 +1,350 @@
+#!/bin/bash
+clear
+color1='\e[031;1m'
+color2='\e[34;1m'
+color3='\e[0m'
+echo -e "\e[36m※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※
+※                                                   ※
+※\e[0m       [WELCOME MY SYSTEM KINGKONGVPN VPS]         \e[36m※
+※                                                   ※
+※\e[0m        \e[32mWhatsApp\e[0m    \e[33m:\e[0m \e[34m+60178760080\e[0m                 \e[36m※
+※\e[0m        \e[32mTelegram\e[0m    \e[33m:\e[0m \e[34m@xiihaiqal\e[0m                   \e[36m※
+※                                                   ※
+※\e[0m         COPYRIGHT \e[31m©\e[0m KINGKONGVPN™ 2021 \e[31m®\e[0m           \e[36m※
+※                                                   ※
+※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※\e[0m
+"
+
+echo -e " ${color1}-----------------=[ V2ray Menu ]=--------------------${color3}"
+        echo -e " 1) Create Vmess Account       5) Create Vless Account"
+        echo -e " 2) Renew Vmess Account        6) Renew Vmess Account"
+        echo -e " 3) Delete Vmess Account       7) Delete Vless Account"
+    echo -e " 4) Renew Vmess certificate    8) Back To Menu"
+echo -e ""
+read -p "$(echo -e "Select from options (1-8): ")" Accounts
+echo -e "\n\n"
+case $Accounts in
+                1)
+domain=$(cat /etc/v2ray/domain)
+  echo -e "\e[36m------------------------------------------\e[0m"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+                  echo -e "\e[36m------------------------------------------\e[0m"
+                read -rp "※ Enter Username : " -e user
+                CLIENT_EXISTS=$(grep -w $user /etc/v2ray/config.json | wc -l)
+
+                if [[ ${CLIENT_EXISTS} == '1' ]]; then
+                        echo ""
+                echo -e "Username \e[31m$user\e[0m Already Register ..!"
+                        exit 1
+                fi
+        done
+uuid=$(cat /proc/sys/kernel/random/uuid)
+read -p "※ Total Days To Exp Or Y-M-D : " masaaktif
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+sed -i '/#tls$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"2"',"email": "'""$user""'"' /etc/v2ray/config.json
+sed -i '/#none$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"2"',"email": "'""$user""'"' /etc/v2ray/none.json
+cat>/etc/v2ray/$user-tls.json<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "4443",
+      "id": "${uuid}",
+      "aid": "2",
+      "net": "ws",
+      "path": "/v2ray",
+      "type": "none",
+      "host": "",
+      "tls": "tls"
+}
+EOF
+cat>/etc/v2ray/$user-none.json<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "80",
+      "id": "${uuid}",
+      "aid": "2",
+      "net": "ws",
+      "path": "/v2ray",
+      "type": "none",
+      "host": "",
+      "tls": "none"
+}
+EOF
+vmesslink1="vmess://$(base64 -w 0 /etc/v2ray/$user-tls.json)"
+vmesslink2="vmess://$(base64 -w 0 /etc/v2ray/$user-none.json)"
+systemctl restart v2ray
+systemctl restart v2ray@none
+clear
+        echo " ";
+        echo " ";
+        echo " ";
+        echo -e " \e[36m※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※
+ ※                                 ※
+ ※\e[0m  WELCOME MY SYSTEM KINGKONGVPN  \e[36m※
+ ※                                 ※
+ ※\e[0m   \e[35mWhatsApp\e[0m   \e[36m:\e[0m \e[33m+60178760080\e[0m     \e[36m※
+ ※\e[0m   \e[35mTelegram\e[0m   \e[36m:\e[0m \e[33m@xiihaiqal\e[0m       \e[36m※
+ ※                                 ※
+ ※\e[0m        \e[31m©\e[0m KINGKONGVPN™ \e[31m®\e[0m         \e[36m※
+ ※                                 ※
+ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※ ※\e[0m";
+echo -e "" 
+echo -e "==========-V2RAY/VMESS-=========="
+echo -e "Remarks        : ${user}"
+echo -e "Domain         : ${domain}"
+echo -e "port TLS       : 4443"
+echo -e "port none TLS  : 80"
+echo -e "id             : ${uuid}"
+echo -e "alterId        : 2"
+echo -e "Security       : auto"
+echo -e "network        : ws"
+echo -e "path           : /v2ray"
+echo -e "================================="
+echo -e "link TLS       : ${vmesslink1}"
+echo -e "================================="
+echo -e "link none TLS  : ${vmesslink2}"
+echo -e "================================="
+echo -e "Expired On     : $exp"
+echo -e "By KingKongVPN"
+
+                                exit
+                ;;
+                2)
+                clear
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/v2ray/config.json")
+        if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+                clear
+                echo ""
+                echo "You have no existing clients!"
+                exit 1
+        fi
+
+        clear
+        echo ""
+        echo "Select the existing client you want to renew"
+        echo " Press CTRL+C to return"
+        echo -e "==============================="
+        grep -E "^### " "/etc/v2ray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
+        until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+                if [[ ${CLIENT_NUMBER} == '1' ]]; then
+                        read -rp "Select one client [1]: " CLIENT_NUMBER
+                else
+                        read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+                fi
+        done
+read -p "Expired (days): " masaaktif
+user=$(grep -E "^### " "/etc/v2ray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/etc/v2ray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+now=$(date +%Y-%m-%d)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+exp3=$(($exp2 + $masaaktif))
+exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/v2ray/config.json
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/v2ray/none.json
+clear
+echo ""
+echo " VMESS account successfully renewed"
+echo " =========================="
+echo " Client Name : $user"
+echo " Expired On  : $exp4"
+echo " =========================="
+echo " By KongKongVPN"
+
+                exit
+                ;;
+                3)
+                clear
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/v2ray/config.json")
+        if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+                echo ""
+                echo "You have no existing clients!"
+                exit 1
+        fi
+
+        clear
+        echo ""
+        echo " Select the existing client you want to remove"
+        echo " Press CTRL+C to return"
+        echo " ==============================="
+        echo "     No  Expired   User"
+        grep -E "^### " "/etc/v2ray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
+        until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+                if [[ ${CLIENT_NUMBER} == '1' ]]; then
+                        read -rp "Select one client [1]: " CLIENT_NUMBER
+                else
+                        read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+                fi
+        done
+user=$(grep -E "^### " "/etc/v2ray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/etc/v2ray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+sed -i "/^### $user $exp/,/^},{/d" /etc/v2ray/config.json
+sed -i "/^### $user $exp/,/^},{/d" /etc/v2ray/none.json
+rm -f /etc/v2ray/$user-tls.json /etc/v2ray/$user-none.json
+systemctl restart v2ray
+systemctl restart v2ray@none
+clear
+echo " V2RAY Account Deleted Successfully"
+echo " =========================="
+echo " Client Name : $user"
+echo " Expired On  : $exp"
+echo " =========================="
+echo " By KongKongVPN"
+                exit
+                ;;
+                4)
+                clear
+echo start
+sleep 0.5
+domain=$(cat /etc/v2ray/domain)
+systemctl stop v2ray
+systemctl stop v2ray@none
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
+systemctl start v2ray
+systemctl start v2ray@none
+echo Done
+sleep 0.5
+clear 
+                exit
+                ;;
+                5)
+                clear
+domain=$(cat /etc/v2ray/domain)
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+                  echo -e "\e[36m------------------------------------------\e[0m"
+                read -rp "※ Enter Username : " -e user
+                CLIENT_EXISTS=$(grep -w $user /etc/v2ray/vless.json | wc -l)
+
+                if [[ ${CLIENT_EXISTS} == '1' ]]; then
+                        echo ""
+                echo -e "Username \e[31m$user\e[0m Already Register ..!"
+                        exit 1
+                fi
+        done
+uuid=$(cat /proc/sys/kernel/random/uuid)
+read -p "※ Enter Password : " masaaktif
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+sed -i '/#tls$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","email": "'""$user""'"' /etc/v2ray/vless.json
+sed -i '/#none$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","email": "'""$user""'"' /etc/v2ray/vnone.json
+vlesslink1="vless://${uuid}@${domain}:5443?path=/v2ray&security=tls&encryption=none&type=ws#${user}"
+vlesslink2="vless://${uuid}@${domain}:880?path=/v2ray&encryption=none&type=ws#${user}"
+systemctl restart v2ray@vless
+systemctl restart v2ray@vnone
+clear
+echo -e ""
+echo -e "==========-V2RAY/VLESS-=========="
+echo -e "Remarks        : ${user}"
+echo -e "Domain         : ${domain}"
+echo -e "port TLS       : 5443"
+echo -e "port none TLS  : 880"
+echo -e "id             : ${uuid}"
+echo -e "Encryption     : none"
+echo -e "network        : ws"
+echo -e "path           : /v2ray"
+echo -e "================================="
+echo -e "link TLS       : ${vlesslink1}"
+echo -e "================================="
+echo -e "link none TLS  : ${vlesslink2}"
+echo -e "================================="
+echo -e "Expired On     : $exp"
+echo -e "By KingKongVPN"
+
+                exit
+                ;;
+                6)
+                clear
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/v2ray/vless.json")
+        if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+                clear
+                echo ""
+                echo "You have no existing clients!"
+                exit 1
+        fi
+
+        clear
+        echo ""
+        echo "Select the existing client you want to renew"
+        echo " Press CTRL+C to return"
+        echo -e "==============================="
+        grep -E "^### " "/etc/v2ray/vless.json" | cut -d ' ' -f 2-3 | nl -s ') '
+        until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+                if [[ ${CLIENT_NUMBER} == '1' ]]; then
+                        read -rp "Select one client [1]: " CLIENT_NUMBER
+                else
+                        read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+                fi
+        done
+read -p "Expired (days): " masaaktif
+user=$(grep -E "^### " "/etc/v2ray/vless.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/etc/v2ray/vless.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+now=$(date +%Y-%m-%d)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+exp3=$(($exp2 + $masaaktif))
+exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/v2ray/vless.json
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/v2ray/vnone.json
+clear
+echo ""
+echo " VLESS Account Was Successfully Renewed"
+echo " =========================="
+echo " Client Name : $user"
+echo " Expired On  : $exp4"
+echo " =========================="
+echo " By KongKongVPN"
+                exit
+                ;;
+                7)
+                clear
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/v2ray/vless.json")
+        if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+                echo ""
+                echo "You have no existing clients!"
+                exit 1
+        fi
+
+        clear
+        echo ""
+        echo " Select the existing client you want to remove"
+        echo " Press CTRL+C to return"
+        echo " ==============================="
+        echo "     No  Expired   User"
+        grep -E "^### " "/etc/v2ray/vless.json" | cut -d ' ' -f 2-3 | nl -s ') '
+        until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+                if [[ ${CLIENT_NUMBER} == '1' ]]; then
+                        read -rp "Select one client [1]: " CLIENT_NUMBER
+                else
+                        read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+                fi
+        done
+user=$(grep -E "^### " "/etc/v2ray/vless.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/etc/v2ray/vless.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+sed -i "/^### $user $exp/,/^},{/d" /etc/v2ray/vless.json
+sed -i "/^### $user $exp/,/^},{/d" /etc/v2ray/vnone.json
+systemctl restart v2ray@vless
+systemctl restart v2ray@none
+clear
+echo " V2RAY Account Deleted Successfully"
+echo " =========================="
+echo " Client Name : $user"
+echo " Expired On  : $exp"
+echo " =========================="
+echo -e "By KongKongVPN"
+                exit
+                ;;
+                8)
+                clear
+                menu
+                exit
+                ;;
+        esac
